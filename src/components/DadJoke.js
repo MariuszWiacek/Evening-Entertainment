34ce4268f52059520f73658e4f8b168a19240326
MariuszWiacek <wiacek.mariusz@gmail.com>
2023-03-15 19:14:20 +0000

    jokes card added

--- src/components/DadJoke.js
+++ src/components/DadJoke.js
@@ -1,11 +1,41 @@
-import React from 'react';
 
-const DadJoke = () =>{
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./jokes.css";
+
+ 
+
+
+function DadJoke() {
+  const [joke, setJoke] = useState("");
+ 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const requestURL = `https://api.api-ninjas.com/v1/dadjokes?limit=`;
+
+      const requestURLconfig = {
+        headers: {
+          "X-Api-Key": "R18SX0AP35jxfpCm/bfW1A==eed69WOIbzU6Hyhv",
+        },
+      };
+      const response = await axios(requestURL, requestURLconfig);
+      const joke = response["data"][0].joke;
+     
+      setJoke(joke);
+    
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>DadJoke section</div>
-   
-
-  )
+    <div>
+      <p>Joke of the day: </p>
+      <p className="jokes">{joke}</p>
+      
+     
+      <p></p>
+    </div>
+  );
 }
-
 export default DadJoke
