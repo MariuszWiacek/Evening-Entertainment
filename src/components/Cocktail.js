d29af6dfcced743658c8853466ac30c8c68d6520
mihacuo <mihhail.odnolko@gmail.com>
2023-03-15 16:04:00 +0000

    added cocktail section

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -2,8 +2,25 @@ import React, { useState } from "react";
 import axios from "axios";
 import './Cocktail.css'
 
+const arrayOfCocktails = [
+  'Aviation',
+  'Bijou',
+  'Bramble',
+  'Clover Club',
+  'Corpse Reviver',
+  'Bloody Mary',
+  'Hot Toddy',
+  'Whiskey Sour',
+  'Whiskey Smash',
+  'Hurricane'
+ ]
 
-const requestURL = "https://api.api-ninjas.com/v1/cocktail?name=bloody mary";
+const randomCocktail =arrayOfCocktails[Math.floor(Math.random()*arrayOfCocktails.length)];
+console.log(randomCocktail)
+//const randomCocktail = "Bloody Mary"
+const requestURL = `https://api.api-ninjas.com/v1/cocktail?name=${randomCocktail}`;
+
+
 
 const requestURLconfig = {
   headers: {
@@ -11,24 +28,27 @@ const requestURLconfig = {
   },
 };
 
-let name, instructions;
-
 function Cocktail() {
   const [cocktail, setCocktail] = useState("");
   const [instructions, setInstructions] = useState("");
+  const [ingridients, setingridients] = useState("");
   axios.get(requestURL, requestURLconfig).then(function (response) {
     // handle success
     // console.log(response["data"][0]);
     const name = response["data"][0].name;
     const instr = response["data"][0].instructions;
+    const ingr = response["data"][0].ingredients;
     console.log(name);
-    console.log(instr)
+    console.log(ingr);
+    console.log(instr);
     setCocktail(name)
     setInstructions(instr)
+    setingridients(ingr)
   });
   return (
     <div>
       <p className="cocktail-name">Cocktail: {cocktail}</p>
+      <p className="cocktail-ingridients">Ingridients: {ingridients}</p>
       <p className="cocktail-instructions">Instructions: {instructions}</p>
       <p></p>
     </div>
@@ -36,3 +56,4 @@ function Cocktail() {
 }
 
 export default Cocktail;
+
