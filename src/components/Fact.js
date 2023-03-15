475f532779e876af05987d1b1bfd0cd08700a08a
NatCro <njcrockwell@gmail.com>
2023-03-15 19:01:54 +0000

    added fact code that displays in the card using an api call

--- src/components/Fact.js
+++ src/components/Fact.js
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Fact = () =>{
+
+
+
+// var limit = 1;
+// fetch('https://api.api-ninjas.com/v1/facts?limit=' + limit, {
+//   headers: {
+//     'X-Api-Key': '+HjQRWo/G88Py6vuvVWDeA==Soa7hk2iKIwOlJnt'
+//   }
+// })
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => console.error('Error:', error));
+
+
+
+
+// const Fact = () =>{
+//   return (
+    
+//       <p>
+//           Fact section
+//       </p>
+    
+
+    
+//   )
+// }
+
+
+
+
+const Fact = () => {
+  const [fact, setFact] = useState('');
+
+  useEffect(() => {
+    const fetchFact = async () => {
+      const response = await fetch('https://api.api-ninjas.com/v1/facts?limit=1', {
+        headers: {
+          'X-Api-Key': '+HjQRWo/G88Py6vuvVWDeA==Soa7hk2iKIwOlJnt'
+        }
+      });
+      const data = await response.json();
+      setFact(data[0].fact);
+    };
+    fetchFact();
+  }, []);
+
   return (
-    
-      <p>
-          Fact section
-      </p>
-    
-
-    
-  )
+    <p>
+      {fact}
+    </p>
+  );
 }
+
+
+
+
+
+
 
 export default Fact
