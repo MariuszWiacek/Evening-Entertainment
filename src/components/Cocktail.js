c4c29c343688f9156ba089f87fdc0ed1d37f1bd9
mihacuo <mihhail.odnolko@gmail.com>
2023-03-15 18:26:29 +0000

    changed cocktail

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -29,22 +29,37 @@ const requestURLconfig = {
 };
 
 function Cocktail() {
-  const [cocktail, setCocktail] = useState("");
-  const [instructions, setInstructions] = useState("");
-  const [ingridients, setingridients] = useState("");
-  axios.get(requestURL, requestURLconfig).then(function (response) {
-    // handle success
-    // console.log(response["data"][0]);
-    const name = response["data"][0].name;
-    const instr = response["data"][0].instructions;
-    const ingr = response["data"][0].ingredients;
-    console.log(name);
-    console.log(ingr);
-    console.log(instr);
-    setCocktail(name)
-    setInstructions(instr)
-    setingridients(ingr)
-  });
+  // const [cocktail, setCocktail] = useState("");
+  // const [instructions, setInstructions] = useState("");
+  // const [ingridients, setingridients] = useState("");
+  // axios.get(requestURL, requestURLconfig).then(function (response) {
+  //   // handle success
+  //   // console.log(response["data"][0]);
+
+  //   const name = "bijou";
+  //   const instr = "Stir, strain, straight up, cocktail glass, garnish.";
+  //   const ingr = ['1 oz Gin', '1 oz Sweet vermouth', '1 oz Herbal liqueur, Green Chartreuse', '1 ds Orange bitters', '1  Maraschino cherry (as garnish)']
+
+
+
+  //   // const name = response["data"][0].name;
+  //   // const instr = response["data"][0].instructions;
+  //   // const ingr = response["data"][0].ingredients;
+
+  //   console.log(name);
+  //   console.log(ingr);
+  //   console.log(instr);
+  //   setCocktail(name)
+  //   setInstructions(instr)
+  //   setingridients(ingr)
+  // });
+
+  
+  const cocktail = "bijou";
+  const ingridients = "Stir, strain, straight up, cocktail glass, garnish.";
+  const instructions = ['1 oz Gin', '1 oz Sweet vermouth', '1 oz Herbal liqueur, Green Chartreuse', '1 ds Orange bitters', '1  Maraschino cherry (as garnish)']
+
+
   return (
     <div>
       <p className="cocktail-name">Cocktail: {cocktail}</p>
