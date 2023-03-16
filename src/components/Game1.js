9bbeacb414460d10415cb5542dfb9847914ceba0
mihacuo <mihhail.odnolko@gmail.com>
2023-03-16 18:01:30 +0000

    small changes to Game1, no more errors

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -77,32 +77,12 @@ function Game1() {
     setCurrentMove(nextHistory.length - 1);
   }
 
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    // return (
-    //   <li key={move}>
-    //     <button onClick={() => jumpTo(move)}>{description}</button>
-    //   </li>
-    // );
-  });
-
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
-      </div>
+
     </div>
   );
 }
