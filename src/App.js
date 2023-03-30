fbf4c057590d4a73d67a68764bd9edb099c12217
MariuszWiacek <wiacek.mariusz@gmail.com>
2023-03-30 08:04:44 +0100

    swipeable

--- src/App.js
+++ src/App.js
@@ -13,67 +13,66 @@ import Video from "./components/video";
 import Cook from "./components/cook"
 import Swipeable from 'react-swipeable';
 
-// import Intro from "./components/intro";
-// import ReactDOM from "react-dom";
-
-
 function App() {
+
+  function handleSwipeLeft() {
+    document.querySelector('#sliderify').sliderifyNextSlide();
+  }
+
+  function handleSwipeRight() {
+    document.querySelector('#sliderify').sliderifyPreviousSlide();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <div class="jumbotron">
           <div className="title"><h1 style={{ fontSize: "70px" }}>Evening Entertainment</h1>
-
             <p className="subtitle" style={{ fontSize: "20px", color: "orange", textShadow: 'none'}}>
               A place for all your evening entertainment
             </p>
-
           </div>
         </div>
 
-
         <div id="slider" style={{height: '700px', width:'1200px'}}>
-        <Swipeable onSwipedLeft={handleSwipeLeft} onSwipedRight={handleSwipeRight}>
-          <Sliderify  slideDurationInSecs="250" stopOnCurrent={true}>
-            <div>
-              <Party />
-            </div>
-            <div>
-              <Fact />
-            </div>
-            <div>
-              <Cook />
-            </div>
-            <div>
-              <Cocktail />
-            </div>
-            <div>
-              <Movie />
-            </div>
-            <div>
-              <Game1 />
-            </div>
-            <div>
-              <Video />
-            </div>
-            <div>
-              <DadJoke />
-            </div>
-            <div>
-              <Giphy />
-            </div>
-            <div>
-              <Email />
-            </div>
-          </Sliderify>
+          <Swipeable onSwipedLeft={handleSwipeLeft} onSwipedRight={handleSwipeRight}>
+            <Sliderify slideDurationInSecs="250" stopOnCurrent={true} id="sliderify">
+              <div>
+                <Party />
+              </div>
+              <div>
+                <Fact />
+              </div>
+              <div>
+                <Cook />
+              </div>
+              <div>
+                <Cocktail />
+              </div>
+              <div>
+                <Movie />
+              </div>
+              <div>
+                <Game1 />
+              </div>
+              <div>
+                <Video />
+              </div>
+              <div>
+                <DadJoke />
+              </div>
+              <div>
+                <Giphy />
+              </div>
+              <div>
+                <Email />
+              </div>
+            </Sliderify>
           </Swipeable>
-
         </div>
-
 
       </header>
     </div>
-
   );
 }
 
